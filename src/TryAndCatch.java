0907ad99b62fa4d225f358c498b640a34488e0a8
Jaydeep <jaydeep.cwj@gmail.com>
2023-10-29 23:58:05 +0530

    Try and Catch

--- src/TryAndCatch.java
+++ src/TryAndCatch.java
@@ -6,19 +6,19 @@ public class TryAndCatch {
         int b = 0;
 
 //        Without Try
-//        int c = a/b;
-//        System.out.println(c);
+        int c = a/b;
+        System.out.println(c);
 
 //        With Try
-        try{
-            int c = a/b;
-            System.out.println(c);
-        }
-        catch (Exception e){
-            System.out.println("We failed to drive .Reason:");
-            System.out.println(e);
-        }
-        System.out.println("End of the Program");
+//        try{
+//            int c = a/b;
+//            System.out.println(c);
+//        }
+//        catch (Exception e){
+//            System.out.println("We failed to drive .Reason:");
+//            System.out.println(e);
+//        }
+//        System.out.println("End of the Program");
 
     }
 }
